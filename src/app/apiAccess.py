2ee38bacb4bceebad27f45f7516624a6ab030101
lisa <lis.ye@mail.utoronto.ca>
2024-01-27 19:29:41 -0500

    Changes made

--- src/app/apiAccess.py
+++ src/app/apiAccess.py
@@ -1,16 +1,35 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-def get_songs(year: str, genre: str, vibe: str) -> dict[str: str]:
 
+def get_songs(year: str, country: str) -> dict[str: str]:
+    songs = {}
     scope = "user-library-read"
-
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-    results = sp.search(q=f'genre: {genre}&year{year}', limit=10, type="track")
+
+    # q = String.init(format:"artist:%@ track:%@", artistName, trackName)
+
+    search_query = 'year:' + year + ' genre:' + country
+    results = sp.search(q=search_query)
+    # results = sp.search(q=f'genre: {genre}&year: {year}', limit=10, type="track")
 
     print(results)
+    if len(results["tracks"]["items"]) == 0:
+        return {}
 
-    return {
-        "Tattoo": "Loreen",
-        "Cha Cha Cha": "Kaarjia",
-    }
+    for i in range(0, len(results["tracks"]["items"])):
+        track_name = results["tracks"]["items"][i]["name"]
+        artist = results["tracks"]["items"][i]["artists"][0]["name"]
+        songs[track_name] = artist
+        # explicit = results["tracks"]["items"][i]['explicit']
+
+    # image =
+
+    with open("testestest.json", "w") as text_file:
+        text_file.write(str(results))
+        text_file.close()
+
+    # print(results["tracks"])
+    return songs
+
+# def parse_response(j_response: dict[str: str]) -> None:
--- src/app/main.py
+++ src/app/main.py
@@ -8,6 +8,8 @@ def get_user_input() -> list[str]:
     """
     print("Welcome to GruVee")
 
+    year = ''
+
     year_is_valid = False
 
     while not year_is_valid:
@@ -15,27 +17,28 @@ def get_user_input() -> list[str]:
 
         if not year.isnumeric() or int(year) >= 2030:
             print("Please enter a valid year")
-        else: # year < 2030
+        else:
+            # year < 2030
             year_is_valid = True
 
-    genre = input("Enter a music genre: ")
+    country = input("Enter a country that you miss: ")
 
-    vibe = input("Enter the kind of vibe you want (eg sad): ")
-
-    return [year, genre, vibe]
+    return [year, country]
 
 
 if __name__ == "__main__":
     user_inputs = get_user_input()
 
     userYear = user_inputs[0]
-    userGenre = user_inputs[1].lower()
-    userVibe = user_inputs[2].lower()
+    userGenre = user_inputs[1]
 
-    songs = apiAccess.get_songs(userYear, userGenre, userVibe)
+    songs = apiAccess.get_songs(userYear, userGenre)
 
-    print(f'\nHere are your recommended {userVibe} {userGenre} songs from the '
-          f'{userYear}s:')
+    if len(songs) == 0:
+        print("No songs were found.")
+    else:
+        print(f'\nHere are your recommended {userGenre} songs from the '
+              f'{userYear}s:')
 
-    for song in songs:
-        print(f'{song} - {songs[song]}')
+        for song in songs:
+            print(f'{song} - {songs[song]}')
